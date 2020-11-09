aee420946ee4f8e893db8c020d5ca54aef11f7b6
Sukriti Dawar <55523401+sukritidawar@users.noreply.github.com>
2020-11-09 09:50:54 +0530

    Update iot_project.ino

--- iot_project.ino
+++ iot_project.ino
@@ -13,18 +13,18 @@ void setup() {
 
 void loop() {                         
     if(digitalRead(sensor)){    // Checking the sensor
-    digitalWrite(LED, HIGH);   // turn the LED ON
-    delay(1000);
-    DHT11.read11(DHT11_PIN);
-      if(DHT11_PIN > 30) {
+      digitalWrite(LED, HIGH);   // turn the LED ON
+      delay(1000);
+      DHT11.read11(DHT11_PIN);
+      if(DHT11_PIN > 30) { //check whether temperature is greater than 30 Celcius
         digitalWrite(RELAY_PIN, HIGH); // turn on fan 5 seconds
-         delay(5000);
-        digitalWrite(RELAY_PIN, LOW);  // turn off fan 5 seconds
         delay(5000);
       }
     }
     else{
       digitalWrite(LED, LOW);    // turn the LED OFF
       delay(1000); 
+      digitalWrite(RELAY_PIN, LOW);  // turn off fan 
+      delay(5000);
    }
 }
