96ae1ae899003560cb7a5a2ed8a96ffae76eb4aa
Sukriti Dawar <sukritidawar@gmail.com>
2020-11-09 09:39:41 +0530

    Added the code

---
+++ iot_project.ino
@@ -0,0 +1,30 @@
+#include "dht.h"
+#define DHT11_PIN D7
+dht DHT11;
+int LED = 15;
+int sensor = 12;  // pin number of sensor
+const int RELAY_PIN = A5; 
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(sensor, INPUT); //set sensor as INPUT
+  pinMode(RELAY_PIN, OUTPUT);
+}
+
+void loop() {                         
+    if(digitalRead(sensor)){    // Checking the sensor
+    digitalWrite(LED, HIGH);   // turn the LED ON
+    delay(1000);
+    DHT11.read11(DHT11_PIN);
+      if(DHT11_PIN > 30) {
+        digitalWrite(RELAY_PIN, HIGH); // turn on fan 5 seconds
+         delay(5000);
+        digitalWrite(RELAY_PIN, LOW);  // turn off fan 5 seconds
+        delay(5000);
+      }
+    }
+    else{
+      digitalWrite(LED, LOW);    // turn the LED OFF
+      delay(1000); 
+   }
+}
